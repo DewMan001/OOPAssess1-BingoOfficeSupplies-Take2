6c5f21d67c2ecf81caf12131bdb1dd94c927b437
DewMan001 <deweymanster@gmail.com>
2016-11-07 20:38:46 +0800

    Rebuild: Part one; putting most things back in
    Code and design is in place for the main form. Visual studio needs to be
    re-referenced

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,12 +28,83 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.imgBox_Display = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lBox_ProductsDisplay = new System.Windows.Forms.ListBox();
+            this.lbl_SalePrice = new System.Windows.Forms.Label();
+            this.btn_Sale = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.imgBox_Display)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // imgBox_Display
+            // 
+            this.imgBox_Display.Location = new System.Drawing.Point(347, 35);
+            this.imgBox_Display.Name = "imgBox_Display";
+            this.imgBox_Display.Size = new System.Drawing.Size(137, 132);
+            this.imgBox_Display.TabIndex = 1;
+            this.imgBox_Display.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(344, 170);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Product Price:";
+            // 
+            // lBox_ProductsDisplay
+            // 
+            this.lBox_ProductsDisplay.FormattingEnabled = true;
+            this.lBox_ProductsDisplay.Location = new System.Drawing.Point(12, 12);
+            this.lBox_ProductsDisplay.Name = "lBox_ProductsDisplay";
+            this.lBox_ProductsDisplay.Size = new System.Drawing.Size(123, 264);
+            this.lBox_ProductsDisplay.TabIndex = 0;
+            // 
+            // lbl_SalePrice
+            // 
+            this.lbl_SalePrice.AutoSize = true;
+            this.lbl_SalePrice.Location = new System.Drawing.Point(424, 170);
+            this.lbl_SalePrice.Name = "lbl_SalePrice";
+            this.lbl_SalePrice.Size = new System.Drawing.Size(54, 13);
+            this.lbl_SalePrice.TabIndex = 3;
+            this.lbl_SalePrice.Text = "<VALUE>";
+            // 
+            // btn_Sale
+            // 
+            this.btn_Sale.Location = new System.Drawing.Point(347, 186);
+            this.btn_Sale.Name = "btn_Sale";
+            this.btn_Sale.Size = new System.Drawing.Size(137, 28);
+            this.btn_Sale.TabIndex = 4;
+            this.btn_Sale.Text = "Submit Order";
+            this.btn_Sale.UseVisualStyleBackColor = true;
+            this.btn_Sale.Click += new System.EventHandler(this.btn_Sale_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(496, 288);
+            this.Controls.Add(this.btn_Sale);
+            this.Controls.Add(this.lbl_SalePrice);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.imgBox_Display);
+            this.Controls.Add(this.lBox_ProductsDisplay);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.imgBox_Display)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox imgBox_Display;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox lBox_ProductsDisplay;
+        private System.Windows.Forms.Label lbl_SalePrice;
+        private System.Windows.Forms.Button btn_Sale;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -7,14 +7,161 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
-namespace OOPAssess1_BingoOfficeSupplies_Take2
+namespace OOPAssess1_BingoOfficeSupplies
 {
     public partial class Form1 : Form
     {
         public Form1()
         {
             InitializeComponent();
+
+            //string for the path of products.txt
+            string productsPath = "../../Resources/Products.txt";
+            buildArray(productsPath);
         }
+
+        //the various arrays for the product attributes
+        string[] prodNumArray = new string[20];
+        string[] prodNameArray = new string[20];
+        int[] prodStockArray = new int[20];
+        double[] prodPriceArray = new double[20];
+        string[] prodPicArray = new string[20];
+
+        //an integer for the invoice number
+        int invoiceNum = 000000;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lBox_ProductsDisplay.SelectedIndex = 0;
+        }
+
+
+
+
+        private void lBox_ProductsDisplay_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int selectedItem = lBox_ProductsDisplay.SelectedIndex;
+
+            double salePrice = calcSalePrice(prodPriceArray[selectedItem]);
+
+            lbl_SalePrice.Text = salePrice.ToString();
+
+            //file path for the image
+            string imgPath = "../../Resources/" + prodPicArray[selectedItem];
+
+            imgBox_Display.Image = Image.FromFile(imgPath);
+
+        }
+
+        private double calcSalePrice(double originalPrice)
+        {
+            //setting up a variable to store the mark up
+            double markUp;
+            //calculating the markup
+            markUp = originalPrice * 0.60;
+
+            //creating a variable to keep in the sale price
+            double salePrice;
+            //calculating the sale price
+            salePrice = originalPrice + markUp;
+
+
+            //sending back the sale price
+            return salePrice;
+        }
+
+        public void buildArray(string pathOfFile)
+        {
+            //making a string to read in each line
+            string nextLine;
+
+            //and a string for the file path
+            string filePath;
+            filePath = pathOfFile;
+            MessageBox.Show(filePath);
+
+            //and a streamreader to pipe in the data
+            StreamReader pipeIn;
+
+            //finally a string array to hold the split up line
+            string[] splitString = new string[5];
+
+            //checking if the data file exists
+            if (File.Exists(filePath))
+            {
+                //popping open the text file to read things from it
+                pipeIn = File.OpenText(filePath);
+
+                //reading in the next line
+                nextLine = pipeIn.ReadLine();
+                int count = 0;
+                //as long as the next line in the text file isn't empty...
+                while (nextLine != null)
+                {
+                    //making a temporary array to store the split up line
+                    string[] lineArray = new string[5];
+                    //break up the next line by the commas
+                    lineArray = nextLine.Split(',');
+
+                    //order of the fields: product code, product name, amount of stock, individual sale price, image name
+                    prodNumArray[count] = lineArray[0];
+                    prodNameArray[count] = lineArray[1];
+                    prodStockArray[count] = Int32.Parse(lineArray[2]);
+                    prodPriceArray[count] = double.Parse(lineArray[3]);
+                    prodPicArray[count] = lineArray[4];
+                    nextLine = pipeIn.ReadLine();
+                    count++;
+
+                    lBox_ProductsDisplay.Items.Add(lineArray[1]);
+                }
+
+            }
+
+        }
+
+        private void btn_Sale_Click(object sender, EventArgs e)
+        {
+            //incrementing the invoice number by one
+            int newInvoiceNumber = invoiceNum + 1;
+
+            //pulling in the currently selected item
+            int selectedItem = lBox_ProductsDisplay.SelectedIndex;
+
+            //using that index to pick out which stock to edit
+            int prodStock = prodStockArray[selectedItem];
+
+            //picking up the stock the customer wants
+            int stockDesired = Int32.Parse(Microsoft.VisualBasic.Interaction.InputBox("How much stock is desired?" + newInvoiceNumber, "Stock desired", "1", 0, 0));
+
+
+            //comparing the stockDesired to the stockRemaining
+            if (stockDesired <= prodStock)
+            {
+                MessageBox.Show("There is enough stock");
+
+                //editing the prodStock in the array
+                prodStockArray[selectedItem] = prodStock - stockDesired;
+
+                MessageBox.Show("There are " + prodStockArray[selectedItem] + " of this product left");
+            }
+            else if (stockDesired > prodStock)
+            {
+                MessageBox.Show("There is not enough stock. \n \n You tried to order " + stockDesired + ", but unfortunately we only have " + prodStock + ".");
+            }
+
+            double fullPrice;
+
+        }
+
+
+        private void txt_AmountOrdered_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+
+
     }
 }
