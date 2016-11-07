1cb1bd0eb7d2b9b35dd6e21c36c72b4a5206eba8
DewMan001 <deweymanster@gmail.com>
2016-11-07 21:14:05 +0800

    Rebuild pt.3: salesInvoice class back in place

---
+++ salesInvoice.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPAssess1_BingoOfficeSupplies
+{
+    class SalesInvoice
+    {
+        int userInvoiceNumber;
+        string userProductNumber;
+        int userSalePrice;
+        int userAmtPurchased;
+        int userSaleValue;
+
+        public SalesInvoice(int invoiceNumber, string productNumber, int salePrice, int amtPurchased, int saleValue)
+        {
+            userInvoiceNumber = invoiceNumber;
+            userProductNumber = productNumber;
+            userSalePrice = salePrice;
+            userAmtPurchased = amtPurchased;
+            userSaleValue = saleValue;
+        }
+
+        public SalesInvoice()
+        { }
+
+        public int InvoiceNumber
+        {
+            set { userInvoiceNumber = value; }
+            get { return userInvoiceNumber; }
+        }
+
+        public string ProductNumber
+        {
+            set { userProductNumber = value; }
+            get { return userProductNumber; }
+        }
+
+        public int SalePrice
+        {
+            set { userSalePrice = value; }
+            get { return userSalePrice; }
+        }
+
+        public int AmtPurchased
+        {
+            set { userAmtPurchased = value; }
+            get { return userAmtPurchased; }
+        }
+
+        public int SaleValue
+        {
+            set { userSaleValue = value; }
+            get { return userSaleValue; }
+        }
+
+        public int SaleTotal(int SalePrice, int AmountPurchased)
+        {
+            int TotalPrice;
+
+            TotalPrice = SalePrice * AmountPurchased;
+
+            return TotalPrice;
+        }
+    }
+
+}
